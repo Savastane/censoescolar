436b70b97d094ba63d37122ce647499d8ea92adc
Savastane Santana de Andrade <savastane@hotmail.com>
2020-11-12 09:19:53 -0300

    #model identificacao escolar

---
+++ Caronte/Model/IdentificacaoEscolaModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caronte.Model
+{
+    public class IdentificacaoEscolaModel
+    {
+        public string Regitro { get; set; }
+        public long Codigo { get; set; }
+        public long Situacao { get; set; }
+        public long Zona { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataTermino { get; set; }
+
+
+
+        public void LoadMockup()
+        { 
+        
+        }
+        
+    }
+}
--- Caronte/Program.cs
+++ Caronte/Program.cs
@@ -44,7 +44,7 @@ namespace Caronte
         static dynamic getMockupDados()
         {
             string Json2 = @" [" +
-                           @"  {'Registro':'00', 'Codigo': 12345678, 'Situacao': 1  , 'Nome': 'Savastane Andrade' , 'Zona': 1, 'DataInicio' :'2012-04-23T18:25:43.511Z', 'DataTermino' : '2012-04-23T19:25:43.511Z'  }, " +
+                           @"  {'Registro':'00', 'Codigo': 12345678, 'Situacao': 1  , 'Nome': 'Savastane Andrade' , 'Zona': 1, 'DataInicio' :'  ', 'DataTermino' : '2012-04-23T19:25:43.511Z'  }, " +
                            @"  {'Registro':'00', 'Codigo': 23456780, 'Situacao': 2  , 'Nome': 'Andrade Mensdonça' , 'Zona': 2, 'DataInicio' :'2021-04-23T18:00:43.511Z', 'DataTermino' : '2020-04-23T20:35:43.511Z'  } " +                           
                            @" ]";
 
